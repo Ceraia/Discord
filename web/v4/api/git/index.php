1fd8c04d7ccf818f7c1f9240fbe1d0329b160ba6
Axodouble <53979495+Axodouble@users.noreply.github.com>
2023-08-10 13:50:16 +0200

    Addition test

--- web/v4/api/git/index.php
+++ web/v4/api/git/index.php
@@ -5,27 +5,35 @@ $githubPayload = json_decode(file_get_contents('php://input'), true);
 // Discord webhook URL
 $discordWebhookURL = 'https://discord.com/api/webhooks/1139153354619113482/nRxLUPsVOljqjY8M-riYNIQeNaVEfR1zA3nkROsbYieS7zC4oAOUL9QVxYYs8NFSWCAy';
 
+// Count the number of added, removed, and modified files
+$addedFiles = 0;
+$removedFiles = 0;
+$modifiedFiles = 0;
+
+foreach ($githubPayload['commits'] as $commit) {
+    $addedFiles += count($commit['added']);
+    $removedFiles += count($commit['removed']);
+    $modifiedFiles += count($commit['modified']);
+}
+
+// Prepare the summary text
+$summary = "$addedFiles files added, $removedFiles files removed, $modifiedFiles files modified";
+
 // Prepare Discord webhook payload
 $discordPayload = [
     'embeds' => [
         [
-            //have the title follow this format [UDarkRP_Casino:main] 1 new commit
             'title' => '['.$githubPayload['repository']['name'].':'.$githubPayload['repository']['default_branch'].'] '.
-            // add in the number of commits
             count($githubPayload['commits']).
             ' new commit',
-            'url' => $githubPayload['repository']['html_url'],
-            'description' => // for every commit, add in the commit id and the message with the following format
-            //'[`'.substr($githubPayload['commits'][0]['id'], 0, 7) . '`]('.$githubPayload['commits'][0]['url'].') '. $githubPayload['commits'][0]['message']
-            // loop through all the commits and add them to the description
-            implode("\n", array_map(function ($commit) {
+            'description' => $summary . "\n\n" . implode("\n", array_map(function ($commit) {
                 return sprintf('[`%s`](%s) %s',
                     substr($commit['id'], 0, 7),
                     $commit['url'],
                     $commit['message']
                 );
-            }, $githubPayload['commits']))
-            ,
+            }, $githubPayload['commits'])),
+            'url' => $githubPayload['repository']['html_url'],
             'color' => hexdec('3366cc'), // Color code (blue)
             'author' => [
                 'name' => $githubPayload['sender']['login'],
