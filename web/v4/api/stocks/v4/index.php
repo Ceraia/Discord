54487a17f68c148a2da097a4c6a309087cc31ede
Axodouble <53979495+Axodouble@users.noreply.github.com>
2023-09-08 23:51:43 +0200

    Stock market expansionism.

--- web/v4/api/stocks/v4/index.php
+++ web/v4/api/stocks/v4/index.php
@@ -32,7 +32,6 @@ function fluctuateMarketValue($company)
         $company['market']['strength']['loss']
     ) / 100;
 
-
     // Add the current value and time to the 'previous' values
     $company['market']['value'][] = [
         'value' => round($currentValue + ($currentValue * $fluctuation)),
@@ -52,89 +51,128 @@ function getMarketData($filename)
     }
 }
 
-if (isset($_GET)) {
-    // Is instance set if so continue
-    if (isset($_GET['instance']) && $_GET['instance'] && !isset($_GET['populate'])) {
-        // Get the value of the 'instance' parameter from the URL
-        $instance = $_GET['instance'] ?? null;
+// Create a filtered json string to show to the user that doesn't include the strength values
+function createSystemFilteredJson($marketData)
+{
+    // Create a copy of the market data
+    $filteredMarketData = $marketData;
 
-        if (!$instance) {
-            echo "Please provide an 'instance' parameter.";
-            die();
-        }
-
-        // Define the directory where JSON files will be stored
-        $dataDirectory = 'market_data';
-
-        // Create the directory if it doesn't exist
-        if (!file_exists($dataDirectory)) {
-            mkdir($dataDirectory);
-        }
-
-        // Define the filename based on the instance
-        $filename = $dataDirectory . '/' . $instance . '_market_data.json';
-
-        // Get market data
-        $marketData = getMarketData($filename);
-
-        if (!$marketData) {
-            echo "No market data found for instance: " . $instance;
-            die();
-        }
-        // Check the last update timestamp
-        $lastUpdateTimestamp = $marketData["system_data"]['last_update_timestamp'] ?? 0;
-
-        // Calculate the time elapsed since the last update
-        $timeElapsed = time() - $lastUpdateTimestamp;
-
-        // If more than 900 seconds (15 minute) have elapsed, update prices
-        if ($timeElapsed > 1) {
-            $marketData = updateMarketData($filename, $marketData); // Pass by reference
-        }
-
-        // Return the market data as JSON response
-        header('Content-Type: application/json');
-        echo json_encode($marketData, JSON_PRETTY_PRINT);
+    // Loop through each company
+    foreach ($filteredMarketData['market_data'] as $data => &$company) {
+        // Remove the strength values
+        unset($company['market']['strength']);
     }
-    if (isset($_GET['populate'])) {
-        // Get the value of the 'instance' parameter from the URL
-        $instance = $_GET['instance'] ?? null;
 
-        if (!$instance) {
-            echo "Please provide an 'instance' parameter.";
-            die();
-        }
+    // Remove the system data
+    unset($filteredMarketData['system_data']);
 
-        // Define the directory where JSON files will be stored
-        $dataDirectory = 'market_data';
+    // Return the filtered market data
+    return $filteredMarketData;
+}
 
-        // Create the directory if it doesn't exist
-        if (!file_exists($dataDirectory)) {
-            mkdir($dataDirectory);
-        }
+// Log in audit log
+function logAudit($marketDataInput, $action, $action_data, $actor)
+{
+    $marketDataInput["system_data"]['audit_log'][] = [
+        'action' => $action,
+        'action_data' => $action_data,
+        'actor' => $actor,
+        'time' => time()
+    ];
+    return $marketDataInput;
+}
 
-        // Define the filename based on the instance
-        $filename = $dataDirectory . '/' . $instance . '_market_data.json';
+// Is instance set if so continue
+if (isset($_GET['instance'])) {
+    // Get the value of the 'instance' parameter from the URL
+    $instance = $_GET['instance'] ?? null;
 
-        // Get market data
-        $marketData = getMarketData($filename);
+    if (!$instance) {
+        echo "Please provide an 'instance' parameter.";
+        die();
+    }
 
-        if (!$marketData) {
-            echo "No market data found for instance: " . $instance;
-            die();
-        }
+    // Define the directory where JSON files will be stored
+    $dataDirectory = 'market_data';
 
-        // Check how many more fluctuations are needed to reach 672
-        $fluctuationsNeeded = (673 * 2) - count($marketData['market_data'][0]['market']['value']['previous']);
+    // Create the directory if it doesn't exist
+    if (!file_exists($dataDirectory)) {
+        mkdir($dataDirectory);
+    }
 
-        // For the amount needed, update the market data
-        for ($i = 0; $i < $fluctuationsNeeded; $i++) {
-            $marketData = updateMarketData($filename, $marketData); // Pass by reference
-        }
+    // Define the filename based on the instance
+    $filename = $dataDirectory . '/' . $instance . '_market_data.json';
 
+    // Get market data
+    $marketData = getMarketData($filename);
 
-        // Return the market data as JSON response
-        header('Content-Type: application/json');
-        echo json_encode($marketData, JSON_PRETTY_PRINT);
+    if (!$marketData) {
+        echo "No market data found for instance: " . $instance;
+        die();
+    }
+    // Check the last update timestamp
+    $lastUpdateTimestamp = $marketData["system_data"]['last_update_timestamp'] ?? 0;
+
+    // Calculate the time elapsed since the last update
+    $timeElapsed = time() - $lastUpdateTimestamp;
+
+    // If more than 900 seconds (15 minute) have elapsed, update prices
+    if ($timeElapsed > 1) {
+        $marketData = updateMarketData($filename, $marketData); // Pass by reference
     }
 }
+
+// Check if a id is set
+if (isset($_GET['id'])) {
+    // Filter the market data to only include the company with the given ID
+    $marketData['market_data'] = array_filter($marketData['market_data'], function ($company) {
+        return $company['id'] == $_GET['id'];
+    });
+}
+
+// Populate the market data
+if (isset($_GET['populate'])) {
+    // Get the value of the 'instance' parameter from the URL
+    $instance = $_GET['instance'] ?? null;
+
+    if (!$instance) {
+        echo "Please provide an 'instance' parameter.";
+        die();
+    }
+
+    // Define the directory where JSON files will be stored
+    $dataDirectory = 'market_data';
+
+    // Create the directory if it doesn't exist
+    if (!file_exists($dataDirectory)) {
+        mkdir($dataDirectory);
+    }
+
+    // Define the filename based on the instance
+    $filename = $dataDirectory . '/' . $instance . '_market_data.json';
+
+    // Get market data
+    $marketData = getMarketData($filename);
+
+    if (!$marketData) {
+        echo "No market data found for instance: " . $instance;
+        die();
+    }
+
+    // Fluctuate the market value 100 times
+    for ($i = 0; $i < 100; $i++) {
+        $marketData = updateMarketData($filename, $marketData); // Pass by reference
+    }
+}
+
+// Give the user the option to create a new stock
+
+
+
+
+// Return the market data as JSON response
+header('Content-Type: application/json');
+if (isset($_GET['override'])) {
+    echo json_encode($marketData, JSON_PRETTY_PRINT);
+} else
+    echo json_encode(createSystemFilteredJson($marketData), JSON_PRETTY_PRINT);
