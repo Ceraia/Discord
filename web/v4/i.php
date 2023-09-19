f728fea3df15dfe2642403d7302d0aa98e6b4bab
Axodouble <53979495+Axodouble@users.noreply.github.com>
2023-09-19 19:27:31 +0200

    Update i.php

--- web/v4/i.php
+++ web/v4/i.php
@@ -2,21 +2,18 @@
 $bro = $_SERVER['HTTP_USER_AGENT'];
 //File server for xdbl.dev (v7)
 if (isset($_GET['i'])) {
-    if (strpos($bro, "(compatible; Discordbot/2.0;") == true || strpos($bro, "Intel Mac OS X 11.6;") == true) {
-        // check if the file itself exists, otherwise assume it is registered as file link
-        //if the file has a file extension
+    if ((strpos($bro, "(compatible; Discordbot/2.0;") == true) || (strpos($bro, "Intel Mac OS X 11.6;") == true)) {
         if (strpos($_GET['i'], '.') == true) {
-                $imageMimeType = mime_content_type('x/'.$_GET['i']);
-                // Set appropriate headers
-                header('Content-Type: ' . $imageMimeType);
-                header('Content-Length: ' . filesize('x/'.$_GET['i']));
-    // Output the image content
-    readfile('x/'.$_GET['i']);
+            $imageMimeType = mime_content_type('x/' . $_GET['i']);
+            // Set appropriate headers
+            header('Content-Type: ' . $imageMimeType);
+            header('Content-Length: ' . filesize('x/' . $_GET['i']));
+            // Output the image content
+            readfile('x/' . $_GET['i']);
         } else {
             header("Location: " . file_get_contents('x/' . $_GET['i']));
         }
-        // if (file_exists("x/" . $_GET['i'])) header("Location: " . 'https://xdbl.dev/x/' . $_GET['i']);
-        // else header("Location: " . file_get_contents('x/' . $_GET['i']));
+
         die();
     } else
         header("Location: https://xdbl.dev/?i=" . $_GET['i']);
