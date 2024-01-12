f55bb214c9a2236670366ea8bc1195810eb8ad55
Axodouble <53979495+Axodouble@users.noreply.github.com>
2024-01-12 12:30:56 +0100

    Changes to dnd

--- bot/interactions/commands/platter/dnd.js
+++ bot/interactions/commands/platter/dnd.js
@@ -12,14 +12,32 @@ module.exports = {
   guild: "750209335841390642",
   slashcommand: new SlashCommandBuilder()
     .setName("dnd")
-    .setDefaultMemberPermissions(PermissionsBitField.Flags.Administrator)
     .setDMPermission(false)
-    .setDescription("Dungeons & Dragons commands."),
+    .setDescription("DnD related commands.")
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("dice")
+        .setDescription("Roll a dice.")
+        .addIntegerOption((option) =>
+          option
+            .setName("sides")
+            .setDescription("The amount of sides the dice has.")
+            .setRequired(true)
+        )
+    ),
   /**
    * @param {import("discord.js").CommandInteraction} interaction
    * @param {import("discord.js").Client} client
    */
   async executeSlash(interaction, client) {
     interaction.deferReply({ ephemeral: true });
+    if (interaction.options.getSubcommand() === "roll") {
+      await interaction.editReply(
+        `You rolled a ${
+          Math.floor(Math.random() * interaction.options.getInteger("sides")) +
+          1
+        } on a ${interaction.options.getInteger("sides")}-sided dice.`
+      );
+    }
   },
 };
