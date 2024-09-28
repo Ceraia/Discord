4d64a4680838b34d00a688c3e139b92b31ce9305
Axodouble <git@axodouble.com>
2024-09-28 20:24:25 +0200

    Add onReady event handler

---
+++ events/system/onReady.js
@@ -0,0 +1,13 @@
+const { ActivityType } = require("discord.js");
+
+module.exports = {
+    name: "onReady",
+    once: true,
+    /**
+     * @param {import("discord.js").AnySelectMenuInteraction} interaction
+     * @param {import("@client").BotClient} client
+     */
+    async execute(interaction, client) {
+        client.user.setActivity("with the API", { type: ActivityType.Competing });
+    },
+};
