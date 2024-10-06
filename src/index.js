23548810617678654802941268af2803b455a330
Axodouble <git@axodouble.com>
2024-10-06 16:33:24 +0000

    Refactor Discord login to handle login failures gracefully

--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ const { client } = require("./system");
 const dotenv = require("dotenv");
 dotenv.config();
 
-client.login(process.env.TOKEN_BOT);
+client.login(process.env.TOKEN_BOT)
+    .catch(() => {
+        console.error("Could not login to Discord. Please check your token and try again.");
+        process.exit(1);
+     });
